27b5f359189848d540f62b794fd661c5ef319203
Константин <kuz18rus@ya.ru>
2022-12-08 11:11:56 +0400

    Задача 2 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1() {
         System.out.println("Задача 1");
-for (int i = 0; i <=10; i = i +1){
-    System.out.println(i);
+        for (int i = 0; i <= 10; i = i + 1) {
+            System.out.println(i);
+        }
     }
+        public static void task2() {
+            System.out.println("Задача 2");
+            for (int i = 10; i >= 0; i = i - 1) {
+                System.out.println(i);
+            }
+        }
 
-    }
 }
