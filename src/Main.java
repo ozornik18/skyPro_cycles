32418636b08e1c738e3ae759d5f3923472659487
Константин <kuz18rus@ya.ru>
2022-12-08 11:15:38 +0400

    Задача 3 выполнена

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -15,5 +16,10 @@ public class Main {
                 System.out.println(i);
             }
         }
-
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+    }
 }
