d4145ead7cdf077183a695f7f6c9ec3c2c5a0cc5
Константин <kuz18rus@ya.ru>
2022-12-08 11:22:53 +0400

    Задача 5 выполнена

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -27,6 +28,12 @@ public class Main {
         System.out.println("Задача 4");
         for (int i = 10; i >= -10; i = i - 1) {
             System.out.println(i);
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i < 2097; i = i + 4) {
+            System.out.println(i + "год является високосным");
         }
     }
 }
