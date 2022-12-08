a552d1c82b50741254aed4488dd783559721bd1c
Константин <kuz18rus@ya.ru>
2022-12-08 11:17:26 +0400

    Задача 4 выполнена

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -19,6 +20,12 @@ public class Main {
     public static void task3() {
         System.out.println("Задача 3");
         for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i = i - 1) {
             System.out.println(i);
         }
     }
