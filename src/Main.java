d7d395bb36b8f9e7696b3b924f4d4e50ab2f5780
Константин <kuz18rus@ya.ru>
2022-12-08 11:58:37 +0400

    Задача 10 выполнена

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         task7();
         task8();
         task9();
+        task10();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -70,6 +71,12 @@ public class Main {
             total = total + total/100;
             total = total + contribution;
             System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
+    }
+    public static void task10() {
+        System.out.println("Задача 10");
+        for (int i = 1; i < 11; i++){
+            System.out.println("2*" + i + "=" + i * 2);
         }
     }
 }
